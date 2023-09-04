7a623c0324cb68793e26a9aa2b3c0446e1bdacea
nykcasseano <nykcasseano13@gmail.com>
2023-09-04 10:01:20 -0300

    adding more files to the project

---
+++ ECommerceAWS/lib/productsApp-stack.ts
@@ -0,0 +1,26 @@
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as cdk from "aws-cdk-lib";
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+
+
+export class ProductsAppStack extends cdk.Stack {
+    readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+    
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, "ProductsFetchHandler",{
+            runtime: lambda.Runtime.NODEJS_16_X,
+            functionName: "ProductsFetchHandler",
+            entry: "lambda/products/productsFetchHandler.ts",
+            handler: "handler",
+            memorySize: 128,
+            timeout:cdk.Duration.seconds(5),
+            bundling: {
+                minify: true,
+                sourceMap: false
+            }
+        })
+    }    
+}
