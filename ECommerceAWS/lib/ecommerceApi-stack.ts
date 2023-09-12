d2080a56a57c9be7126ee95223acc7661d52ce96
nykcasseano <nykcasseano13@gmail.com>
2023-09-12 10:13:42 -0300

    adding more files to the project

--- ECommerceAWS/lib/ecommerceApi-stack.ts
+++ ECommerceAWS/lib/ecommerceApi-stack.ts
@@ -1,40 +1,41 @@
-import * as cdk from "aws-cdk-lib";
-import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
-import * as apigateway from " aws-cdk-lib/aws-apigateway";
-import * as cwlogs from " aws-cdk-lib/aws-logs";
-import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as apigateway from "aws-cdk-lib/aws-apigateway",
+import * as cwlogs from "aws-cdk-lib/aws-logs",
+import { Construct } from "constructs"
 
-interface ECommerceApiStackProps extends cdk.StackProps{
-    productsFetchHandler: lambdaNodeJS.NodejsFunction
+interface ECommerceApiStackProps extends cdk.StackProps {
+   productsFetchHandler: lambdaNodeJS.NodejsFunction
 }
 
 export class ECommerceApiStack extends cdk.Stack {
 
-    constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
-        super(scope, id, props);
+   constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
+      super(scope, id, props)
 
-        const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogs")
-        const api = new apigateway.RestApi(this, "ECommerceApi",{
-            restApiName: "ECommerceApi",
-            deployOptions: {
-                acessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
-                acessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
-                    httpMethod: true,
-                    ip: true,
-                    protocol: true,
-                    requestTime: true,
-                    resourcePath: true,
-                    responseLength: true,
-                    status: true,
-                    caller: true,
-                    user: true
-                }),
-            }
-        })
+      const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogs")
+      const api = new apigateway.RestApi(this, "ECommerceApi", {
+         restApiName: "ECommerceApi",
+         deployOptions: {
+            accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+            accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+               httpMethod: true,
+               ip: true,
+               protocol: true,
+               requestTime: true,
+               resourcePath: true,
+               responseLength: true,
+               status: true,
+               caller: true,
+               user: true
+            })
+         }
+      })
 
-        const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
-        // "/products"
-        const productsResource = api.root.addResource("products")
-        productsResource.addMethod("Get",productsFetchIntegration)
-    }
+      const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+
+      // "/products"
+      const productsResource = api.root.addResource("products")
+      productsResource.addMethod("GET", productsFetchIntegration)
+   }
 }
--- ECommerceAWS/lib/productsApp-stack.ts
+++ ECommerceAWS/lib/productsApp-stack.ts
@@ -1,26 +1,31 @@
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as cdk from "aws-cdk-lib";
-import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
-import { Construct } from "constructs";
+//https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda-readme.html
+import * as lambda from "aws-cdk-lib/aws-lambda"
 
+//https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda_nodejs-readme.html
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+
+//https://docs.aws.amazon.com/cdk/api/v2/docs/aws-construct-library.html
+import * as cdk from "aws-cdk-lib"
+
+import { Construct } from "constructs"
 
 export class ProductsAppStack extends cdk.Stack {
-    readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
-    
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+   readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
 
-        this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, "ProductsFetchHandler",{
-            runtime: lambda.Runtime.NODEJS_16_X,
-            functionName: "ProductsFetchHandler",
-            entry: "lambda/products/productsFetchHandler.ts",
+   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props)
+
+      this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, 
+         "ProductsFetchFunction", {
+            functionName: "ProductsFetchFunction",
+            entry: "lambda/products/productsFetchFunction.ts",
             handler: "handler",
             memorySize: 128,
-            timeout:cdk.Duration.seconds(5),
+            timeout: cdk.Duration.seconds(5),
             bundling: {
-                minify: true,
-                sourceMap: false
-            }
-        })
-    }    
+               minify: true,
+               sourceMap: false               
+            },            
+         })
+   }
 }
